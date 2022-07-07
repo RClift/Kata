9558c436f92c5cb57d6252963d6b8a160627c555
RClift <rclift@pdx.edu>
2022-07-06 20:53:49 -0700

    Added kata functionallity

--- PairCoding1/src/main/java/edu/pdx/cs410J/RichardNathan/Kata.java
+++ PairCoding1/src/main/java/edu/pdx/cs410J/RichardNathan/Kata.java
@@ -9,6 +9,42 @@ package edu.pdx.cs410J.RichardNathan;
 public class Kata {
 
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+    Kata temp = new Kata;
+    System.out.println(temp.compute("3003"));
+  }
+
+  public string compute(String arg){
+    String return_string = "";
+    Integer num = Integer.parseInt(arg);
+    if(num % 3 == 0){
+      return_string += "Foo";
+    }
+    if(num % 5 == 0){
+      return_string += "Bar";
+    }
+    if(num % 7 == 0){
+      return_string += "Qix";
+    }
+    for(int i = 0; i < arg.length; i++){
+      if(arg.charAt(i) == '3'){
+        return_string += "Foo";
+      }else if(arg.charAt(i) == '5'){
+        return_string += "Bar";
+      }else if(arg.charAt(i) == '7'){
+        return_string += "Qix";
+      }else if(arg.charAt(i) == '0'){
+        return_string += "*";
+      }
+    }
+    if(return_String == ""){
+      for(int i = 0; i < arg.length; i++) {
+        if(arg.charAt(i) == '0'){
+          return_string += "*";
+        }else{
+          return_string += arg.charAt(i);
+        }
+      }
+    }
+    return return_string;
   }
 }
